5a11e063942dd75a9c317ef074ef6e816d9b8609
marcofrancis <marcofrancis@users.noreply.github.com>
2024-05-01 16:45:10 +0200

    Updated domain name

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Marco Francischello',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'marcofrancis.me',
   author: 'Marco Francischello',
 
   // open graph metadata (optional)
